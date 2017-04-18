1f34b3c24855a7a4c56d8305e8fa7bb85e3f8bf6
liyc1996 <swulll123@163.com>
2017-04-18 13:56:25 +0800

    更新至0.4.0"

--- readme.md
+++ readme.md
@@ -1,98 +1,216 @@
-这个是用纯JS编写的创建动画的小demo,您不用使用jQ这些库，而且它能够兼容目前的主流游览器甚至到较老版本的IE6,你能轻松地添加动画，当前版本0.5。
+# flexAnimation
 
-## 使用方法
+这个是用纯JS编写的轻量级生成动画的库，而且它能够兼容目前的主流游览器甚至到较老版本的IE6。
+
+当前版本0.4.0。
+
+## 快速开始
+
 引入flexibleAnimation.min.js或者flexibleAnimation.js文件，接着只需要下面这几步就能够创建出动画效果了
 
-### 创建animate对象的实例
+### 创建Animate对象的实例
 
 ``` js
 
-	var div=new animate();
+	var myAnimation=flexibleAnimation.create(document.getElementById('test'));
+
+```
+
+flexibleAnimation.create()会返回一个Animatie对象的示例。
+
+### 为这个实例配置动画信息
+
+
+
+``` js
+
+	var myAnimation=flexibleAnimation.create(document.getElementById('test'));
+	myAnimation.set({
+            rules:[
+                'opacity:1',
+                'backgroundColor:#00FF00',
+                'transform:rotate(90deg)',
+                'left:400px',
+                'top:500px'
+            ],
+            duration:1000,
+            easing:'ease'
+     });
+
+```
+
+### 开始动画
+
+
+
+``` js
+
+	var myAnimation=flexibleAnimation.create(document.getElementById('test'));
+	myAnimation.set({
+            rules:[
+                'opacity:1',
+                'backgroundColor:#00FF00',
+                'transform:rotate(90deg)',
+                'left:400px',
+                'top:500px'
+            ],
+            duration:1000,
+            easing:'ease'
+     });
+     myAnimation.start();
+
+```
+
+### 同一段时间进行多个动画
+
+在同一个时间段内，你可以创建多个Animate实例，并分别执行动画。但是为了提升动画的性能，我并不推荐使用这种方式。你应该将这些Animate实例放入动画队列中去执行。目前这个动画队列是同步的，这就意味着放入队列中的动画实例可能都会在同一时间内执行。
+
+
+``` js
+
+var myAnimation=flexibleAnimation.create(document.getElementById('test'));
+	myAnimation.set({
+            rules:[
+                'opacity:1',
+                'backgroundColor:#00FF00',
+                'transform:rotate(90deg)',
+                'left:400px',
+                'top:500px'
+            ],
+            duration:1000,
+            easing:'ease'
+     });
+var myAnimation1=flexibleAnimation.create(document.getElementById('test1'));
+	myAnimation.set({
+            rules:[
+                'opacity:1',
+                'backgroundColor:#00FF00',
+                'transform:rotate(90deg)',
+                'left:400px',
+                'top:500px'
+            ],
+            duration:1500,
+            easing:'ease'
+     });     
+
+var myAnimationList = flexibleAniamtion.createAnimationList(myAnimation,myAnimation1);
+
+myAnimationList.start();
+
+```
+## flexibleAniamtion
+
+
+
+### createAnimation(dom,config)
+
+创建并返回一个Animate对象实例
+
+- dom	可选,这个Animate对象绑定的DOM元素
+
+- config 可选,动画效果的配置信息
+- - rules 数组,描述动画最终的目标信息
+- - easing 字符串,可选 设定动画的速度曲线(可选:'linear'、'ease'(默认)、'easeIn'、'easeOut'、'easeInOut') 
+		   数组-必须为4个数字,前两个数字代表贝塞尔曲线的P1点，后两个数字代表P2点。例如:0.42,0.12,0.23,0.18
+- - duration 动画持续时间，单位为ms	
+
+### createAnimationList(Animate,Animate1,Animate2...)
+创建并返回一个动画队列
+
+``` js
+var myAnimationList = flexibleAniamtion.createAnimationList(myAnimation,myAnimation1);
 
 ```
 
 
-### animate对象的方法
 
-#### init(element)
+## Animate对象的方法
 
-初始化需要产生动画的元素,只需要传入元素对象即可，参数:对象
+### init(domElement)
+
+初始化需要产生动画的DOM元素
+
+- element 必选,dom元素对象
+
+举个栗子：
+
 ``` js
 
-	var div=flexibleAniamtion();
+	var div=flexibleAnimation.create();
 	var test=document.getElementBy('test');
 	div.init(test);
 
 ```
 
-#### callback(func)
 
-创建动画执行完毕后的回调函数,参数:函数
 
-#### start([key:value,key1:value1],easing,duration)
+
+### set(config)
+
+- config 对象,配置动画的信息
+- - rules 数组,描述动画最终的目标信息
+- - easing 字符串,可选 设定动画的速度曲线(可选:'linear'、'ease'(默认)、'easeIn'、'easeOut'、'easeInOut') 
+		   数组-必须为4个数字,前两个数字代表贝塞尔曲线的P1点，后两个数字代表P2点。例如:0.42,0.12,0.23,0.18
+- - duration 动画持续时间，单位为ms
+
+举个例子:
+
+``` js
+var div=flexibleAniamtion.createAnimation(document.getElementById('test'),{
+            rules:[
+                'opacity:1',
+                'backgroundColor:#FF0000',
+                'transform:rotate(90deg)',
+                'left:400px',
+                'top:500px'
+            ],
+            duration:800,
+            easing:'ease'
+        });
+
+```
+
+### start()
 
 开启动画
 
-- [key:value,key1:value1] 
-字符串数组，用于设定动画最终完成的时候元素的属性值。例如:
+
+### callback(func)
+
+创建动画执行完毕后的回调函数
+
+- fun 必选，函数
+
+
+## AnimateList对象的方法
+
+### add(Animate,Animate1,Animate2...)
+
+将一个或者多个Animate的实例放入动画队列当中。
 
 ``` js
-
-	[
-		"color:#161616",
-		"backgroundColor:#ffffff",
-		"left:100px"
-	]
-
+var myAnimationList = flexibleAniamtion.createAnimationList();
+myAnimationList.add(myAnimation,myAnimation1);
 
 ```
 
-- easing
+### delete(Animate)
 
-设定动画的速度曲线 
-
-字符串-'linear'、'ease'、'easeIn'、'easeOut'、'easeInOut'
-
-数组-必须为4个数字,前两个数字代表贝塞尔曲线的P1点，后两个数字代表P2点。例如:0.42,0.12,0.23,0.18
-
-- duration
-
-动画持续时间，单位为ms
-
-#### 示例代码
-
-``` html
-
-<div class='test' id='test' style='left:100px;margin-left:100px;opacity:0;background-color:#333333'></div>
-
-```
-or css
-
-``` css
-.test{
- left:100px;
- margin-left:100px;
- opacity:0;
- background-color:#333333';
-}
-
-```
+删除指定的该动画队列中指定的Animate实例。
 
 ``` js
-
-	var div=new animate();
-	div.init(document.getElementById('test'))
-	div.start([
-		'opacity:1',
-		'left:500px',
-		'top:200px',
-		'backgroundColor:#124565'
-		]
-	,600,'linear');
+var myAnimationList = flexibleAniamtion.createAnimationList();
+myAnimationList.add(myAnimation,myAnimation1);
+myAnimationList.delete(myAnimation1);
 
 ```
 
-## 属性支持
+### start()
+
+执行此动画队列的所有动画
+
+
+## rules支持
 
 - 不支持所有简写的属性(勿用),例如:font,background,border
 - 不完全支持transform:rotate(xdeg)，不过也能产生效果
--- src/flexibleAnimation.js
+++ src/flexibleAnimation.js
@@ -1,262 +1,334 @@
 ﻿//动画函数 0.5 by chen
-//date:2016/11/20 21:47 
-var flexibleAniamtion=(function(){
-	function valueParse(value,property){
-		var result='',
-			pattern;
-		if(property==='transform'){
-			pattern=/[-]{0,}[0-9]{1,}[\.]{0,}[0-9]{0,}([e-][0-9]{0,}){0,}/g;	
-		}
-		if(property==='color'||property==='backgroundColor'){
-			pattern=/[0-9]{1,}/g;
-		}
-		result=value.match(pattern);	
-		return result;
-	}
+//date:2016/11/20 21:47
+(function(window){
+    // 数值解析
+    function valueParse(value,property){
+        var pattern;
+        if(property==='transform'){
+            pattern=/[-]{0,}[0-9]{1,}[\.]{0,}[0-9]{0,}([e-][0-9]{0,}){0,}/g;
+        }
+        if(property==='color'||property==='backgroundColor'){
+            pattern=/[0-9]{1,}/g;
+        }
+        result=value.match(pattern);
+        return result;
+    }
+    //值处理
+    function valueHander(self,startValue,endValue,curTime,duration,flag){
+        var len=startValue.length,
+            value=[];
+        for(var i=0;i<len;i++){
+            if(!flag){
+                value[i]=self.easing(curTime,Number(startValue[i]),Number(endValue[i]),duration);
+            }
+            else{
+                value[i]=Math.floor(self.easing(curTime,Number(startValue[i]),Number(endValue[i]),duration));
+            }
+        }
+        return value.join();
+    }
+    //转换为变形矩阵matrix
+    function toStandardValue(value,property,obj){
+        var doc=document,
+            computedtyle=document.defaultView.getComputedStyle,
+            value;
+        if(!obj){
+            var testDiv=doc.createElement('div');
+            testDiv.style.cssText='height:0px;width:0px;opacity:0;position:absolute;';
+            doc.body.appendChild(testDiv);
+            testDiv.style[property]=value;
+            value=computedtyle(testDiv,null)[property];
+            doc.body.removeChild(testDiv);
+        }
+        else{
+            value=computedtyle(obj,null)[property];
 
-	//值处理
-	function valueHander(self,startValue,endValue,curTime,duration,flag){
-		var len=startValue.length,
-			value=[];
-			for(var i=0;i<len;i++){
-				if(!flag){
-					value[i]=self.easing(curTime,Number(startValue[i]),Number(endValue[i]),duration);	
-				}
-				else{
-					value[i]=Math.floor(self.easing(curTime,Number(startValue[i]),Number(endValue[i]),duration));
-				}
-			}
-		return value.join();
-	}
-
-	//转换为变形矩阵matrix
-	function toStandardValue(value,property,obj){
-		var doc=document,
-			computedtyle=document.defaultView.getComputedStyle,
-			value;
-		if(!obj){
-			var testDiv=doc.createElement('div');
-			testDiv.style.cssText='height:0px;width:0px;opacity:0;position:absolute;';
-			doc.body.appendChild(testDiv);
-			testDiv.style[property]=value;
-			value=computedtyle(testDiv,null)[property];
-			doc.body.removeChild(testDiv);
-		}
-		else{
-			value=computedtyle(obj,null)[property];	
-
-		}
-		return valueParse(value,property);
-	}
-	//获取动画执行的当前阶段的对应的属性值
-	function curHandler(propertyName,self,prefix,curTime,startValue,endValue,duration){
-		if(propertyName==='transform'){
-			return  valueHander(self,startValue,endValue,curTime,duration,false);
-		}
-		if(propertyName==='background-color'){
-			return valueHander(self,startValue,endValue,curTime,duration,true);
-		}
-		else{
-			return self.easing(curTime,startValue,endValue,duration);
-		}
-	}
-	//处理样式规则
-	function ruleHandler(obj,rule){
-		var ruleObj=new Object(),
-			rules=rule.split(':'),
-			suffix,
-			computedtyle=document.defaultView.getComputedStyle(obj,null),
-			result;
-		if(rules[0]=='transform'){
-			ruleObj.prefix='matrix('
-			ruleObj.suffix=')';
-			ruleObj.endValue=toStandardValue(rules[1],rules[0],null);
-			ruleObj.startValue=toStandardValue('',rules[0],obj);
-		}
-		else if(rules[0]=='color'||rules[0]=='backgroundColor'){
-				ruleObj.prefix='rgb(';
-				ruleObj.suffix=')';
-				ruleObj.endValue=toStandardValue(rules[1],rules[0],null);
-				ruleObj.startValue=toStandardValue('',rules[0],obj);
-				if(rules[0]==='backgroundColor'){
-					rules[0]='background-color';
-				}
-		}
-		else{
-			if(suffix=rules[1].match(/[a-zA-Z%]{1,}/)){
-				suffix=rules[1].match(/[a-zA-Z%]{1,}/)[0];
-			}
-			else{
-				suffix='';
-			}
-			ruleObj.prefix='';
-			ruleObj.startValue=parseInt(computedtyle[rules[0]]);
-			ruleObj.endValue=parseInt(rules[1]);
-			ruleObj.suffix=suffix;
-		}
-		ruleObj.name=rules[0];
-		return ruleObj;
-	}
-	//贝塞尔动画时间曲线函数，使用的时候调用其中的solve(x,epsilon)方法，参数x为当前动画执行的时间,参数eplsilon为精确度
-	function UnitBezier(p1x, p1y, p2x, p2y) {
-			this.cx=3.0*p1x;
-			this.bx=3.0*(p2x-p1x)-this.cx;
-			this.ax=1.0-this.cx-this.bx;
-			this.cy=3.0*p1y;
-			this.by=3.0*(p2y-p1y)-this.cy;
-			this.ay=1.0-this.cy-this.by;
-	}
-	UnitBezier.prototype = {
-		constructor:UnitBezier,
-		epsilon : 1e-2,  // 默认精度
-		sampleCurveX:function(t) {//贝赛尔曲线t时刻的坐标点的X坐标
-		   	return ((this.ax*t+this.bx)*t+this.cx)*t;
-		},
-		sampleCurveY:function(t){
-		   	return ((this.ay*t+this.by)*t+this.cy)*t;
-		},
-		sampleCurveDerivativeX:function(t) {//贝赛尔曲线t时刻的坐标点的Y坐标
-		    return (3.0*this.ax*t+2.0*this.bx)*t+this.cx;
-		},
-		solveCurveX:function(x,epsilon) {
-				var t0,
-					t1,
-					t2,
-					x2,
-					d2,
-					i;
-				for (t2=x,i=0;i<8;i++) {
-				    x2= this.sampleCurveX(t2)-x;
-				    if(Math.abs (x2)<epsilon)
-				        return t2;
-				    d2=this.sampleCurveDerivativeX(t2);
-				    if(Math.abs(d2)<epsilon)
-				        break;
-				    t2=t2-x2/d2;
-				}
-				t0=0.0;
-				t1=1.0;
-				t2=x;
-				if(t2<t0) return t0;
-				if(t2>t1) return t1;
-				while (t0<t1) {
-					x2=this.sampleCurveX(t2);
-					if (Math.abs(x2-x)<epsilon)
-						return t2;
-					if (x>x2) t0=t2;
-					else t1=t2;
-					t2=(t1-t0)*.5+t0;
-				}
-				return t2;
-		},
-		solve:function(x,epsilon) {
-		   	return this.sampleCurveY(this.solveCurveX(x,epsilon));
-		}
-	}
-	var defaultPattern={
-			ease:new UnitBezier(0.25,0.1,0.25,1),
-			linear:new UnitBezier(0,0,1,1),
-			easeIn:new UnitBezier(0.42,0,1,1),
-			easeOut:new UnitBezier(0,0,0.58,1),
-			easeInOut:new UnitBezier(0.42,0,0.58,1)
-	}
-	var speedPattern=function(){
-		if(arguments.length===1&&typeof(arguments[0])==='string'){
-			var Bezier=defaultPattern[arguments[0]];
-		}
-		else if(arguments[0].length===4){
-			var Bezier=new UnitBezier(arguments[0][0],arguments[0][1],arguments[0][2],arguments[0][3]);
-			for(var i=0;i<4;i++){
-				if(typeof(arguments[0][i])!='number'||arguments[0][i]<0||arguments[0][i]>1){
-					var Bezier=defaultPattern['linear'];
-				}
-			}
-		}
-		return function(curTime,startValue,endValue,duration){
-			return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
-		}
-	}
-	function animate(){
-		this.dom=null;
-		this.startTime=0;
-		this.startValue=[];
-		this.endValue=[];
-		this.propertyName=[];
-		this.suffix=[];
-		this.easing=null;
-		this.duration=null;
-		this.func=null;
-		this.args=null;
-		this.prefix=[];
-	}
-	animate.prototype={
-		constructor:animate,
-		init:function(dom){
-			this.dom=dom;
-		},
-		start:function(rules,duration,easing){
-			this.startTime=+new Date;
-			this.duration=duration;
-			this.easing=speedPattern(easing);
-			var self=this;
-			for(var i=0,len=rules.length;i<len;i++){
-				var ruleDate=ruleHandler(self.dom,rules[i]);
-				self.prefix[i]=ruleDate.prefix;
-				self.suffix[i]=ruleDate.suffix;
-				self.propertyName[i]=ruleDate.name;
-				self.endValue[i]=ruleDate.endValue;
-				self.startValue[i]=ruleDate.startValue;
-			}
-			var go=function(){
-				var t=+new Date(),
-					result='';
-				if(t>=self.startTime+self.duration){
-					for(var i=0,len=rules.length;i<len;i++){
-						result=self.propertyName[i]+':'+self.prefix[i]+self.endValue[i]+self.suffix[i]+';'+result;		
-					}
-					self.dom.style.cssText=result;
-					if(self.func){
-						self.func(self.args);
-					}
-					return false;
-				}
-				else{
-					var cur=[];
-					for(var i=0,len=rules.length;i<len;i++){
-							cur=curHandler(self.propertyName[i],self,self.prefix[i],t-self.startTime,self.startValue[i],self.endValue[i],self.duration);
-							result=self.propertyName[i]+':'+self.prefix[i]+cur+self.suffix[i]+';'+result;
-					}	
-					self.dom.style.cssText=result;
-					requestAnimationFrame(function(){
-						go();
-					});
-				}
-			};
-			go();
-		},
-		callback:function(){
-			this.func=Array.prototype.shift.apply(arguments);
-			this.args=arguments;
-		}
-	}
-	return function(){
-		return new animate();
-	}
-})(); 	
+        }
+        return valueParse(value,property);
+    }
+    //获取动画执行的当前阶段的对应的属性值
+    function curHandler(propertyName,self,prefix,curTime,startValue,endValue,duration){
+        if(propertyName==='transform'){
+            return  valueHander(self,startValue,endValue,curTime,duration,false);
+        }
+        if(propertyName==='background-color'){
+            return valueHander(self,startValue,endValue,curTime,duration,true);
+        }
+        else{
+            return self.easing(curTime,startValue,endValue,duration);
+        }
+    }
+    //处理样式规则
+    function ruleHandler(obj,rule){
+        var ruleObj=new Object(),
+            rules=rule.split(':'),
+            suffix,
+            computedtyle=document.defaultView.getComputedStyle(obj,null),
+            result;
+        if(rules[0]=='transform'){
+            ruleObj.prefix='matrix('
+            ruleObj.suffix=')';
+            ruleObj.endValue=toStandardValue(rules[1],rules[0],null);
+            ruleObj.startValue=toStandardValue('',rules[0],obj);
+        }
+        else if(rules[0]=='color'||rules[0]=='backgroundColor'){
+            ruleObj.prefix='rgb(';
+            ruleObj.suffix=')';
+            ruleObj.endValue=toStandardValue(rules[1],rules[0],null);
+            ruleObj.startValue=toStandardValue('',rules[0],obj);
+            if(rules[0]==='backgroundColor'){
+                rules[0]='background-color';
+            }
+        }
+        else{
+            if(suffix=rules[1].match(/[a-zA-Z%]{1,}/)){
+                suffix=rules[1].match(/[a-zA-Z%]{1,}/)[0];
+            }
+            else{
+                suffix='';
+            }
+            ruleObj.prefix='';
+            ruleObj.startValue=parseInt(computedtyle[rules[0]]);
+            ruleObj.endValue=parseInt(rules[1]);
+            ruleObj.suffix=suffix;
+        }
+        ruleObj.name=rules[0];
+        return ruleObj;
+    }
+    //贝塞尔动画时间曲线函数，使用的时候调用其中的solve(x,epsilon)方法，参数x为当前动画执行的时间,参数eplsilon为精确度
+    function UnitBezier(p1x, p1y, p2x, p2y) {
+        this.cx=3.0*p1x;
+        this.bx=3.0*(p2x-p1x)-this.cx;
+        this.ax=1.0-this.cx-this.bx;
+        this.cy=3.0*p1y;
+        this.by=3.0*(p2y-p1y)-this.cy;
+        this.ay=1.0-this.cy-this.by;
+    }
+    UnitBezier.prototype = {
+        constructor:UnitBezier,
+        epsilon : 1e-2,  // 默认精度
+        sampleCurveX:function(t) {//贝赛尔曲线t时刻的坐标点的X坐标
+            return ((this.ax*t+this.bx)*t+this.cx)*t;
+        },
+        sampleCurveY:function(t){
+            return ((this.ay*t+this.by)*t+this.cy)*t;
+        },
+        sampleCurveDerivativeX:function(t) {//贝赛尔曲线t时刻的坐标点的Y坐标
+            return (3.0*this.ax*t+2.0*this.bx)*t+this.cx;
+        },
+        solveCurveX:function(x,epsilon) {
+            var t0,
+                t1,
+                t2,
+                x2,
+                d2,
+                i;
+            for (t2=x,i=0;i<8;i++) {
+                x2= this.sampleCurveX(t2)-x;
+                if(Math.abs (x2)<epsilon)
+                    return t2;
+                d2=this.sampleCurveDerivativeX(t2);
+                if(Math.abs(d2)<epsilon)
+                    break;
+                t2=t2-x2/d2;
+            }
+            t0=0.0;
+            t1=1.0;
+            t2=x;
+            if(t2<t0) return t0;
+            if(t2>t1) return t1;
+            while (t0<t1) {
+                x2=this.sampleCurveX(t2);
+                if (Math.abs(x2-x)<epsilon)
+                    return t2;
+                if (x>x2) t0=t2;
+                else t1=t2;
+                t2=(t1-t0)*.5+t0;
+            }
+            return t2;
+        },
+        solve:function(x,epsilon) {
+            return this.sampleCurveY(this.solveCurveX(x,epsilon));
+        }
+    }
+    var defaultPattern={
+        ease:new UnitBezier(0.25,0.1,0.25,1),
+        linear:new UnitBezier(0,0,1,1),
+        easeIn:new UnitBezier(0.42,0,1,1),
+        easeOut:new UnitBezier(0,0,0.58,1),
+        easeInOut:new UnitBezier(0.42,0,0.58,1)
+    }
+    var speedPattern=function(){
+        if(arguments.length===1&&typeof(arguments[0])==='string'){
+            var Bezier=defaultPattern[arguments[0]];
+        }
+        else if(arguments[0].length===4){
+            var Bezier=new UnitBezier(arguments[0][0],arguments[0][1],arguments[0][2],arguments[0][3]);
+            for(var i=0;i<4;i++){
+                if(typeof(arguments[0][i])!='number'||arguments[0][i]<0||arguments[0][i]>1){
+                    var Bezier=defaultPattern['linear'];
+                }
+            }
+        }
+        return function(curTime,startValue,endValue,duration){
+            return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
+        }
+    }
+    // animated对象
+    function Animate(){
+        this.dom=null;
+        this.startValue=[];
+        this.endValue=[];
+        this.propertyName=[];
+        this.suffix=[];
+        this.easing=null;
+        this.duration=null;
+        this.func=null;
+        this.args=null;
+        this.prefix=[];
+        this.rules = [];
+    }
+    Animate.prototype={
+        constructor:Animate,
+        init:function(dom){
+            this.dom=dom;
+        },
+        handleCur:function (curTime,startTime) {
+            var result = '',
+                endTime = startTime + this.duration;
+            if(curTime>=endTime){
+                for(var i=0,len=this.rules.length;i<len;i++){
+                    result=this.propertyName[i]+':'+this.prefix[i]+this.endValue[i]+this.suffix[i]+';'+result;
+                }
+                this.dom.style.cssText=result;
+                this.startTime = 0;
+                if(this.func){
+                    this.func(this.args);
+                }
+                return false;
+            }
+            else{
+                var cur=[];
+                for(var i=0,len=this.rules.length;i<len;i++){
+                    cur=curHandler(this.propertyName[i],this,this.prefix[i],curTime-startTime,this.startValue[i],this.endValue[i],this.duration);
+                    result=this.propertyName[i]+':'+this.prefix[i]+cur+this.suffix[i]+';'+result;
+                }
+                this.dom.style.cssText=result;
+                return true;
+            }
+        },
+        set:function (config) {
+            var rules = config.rules,
+                duration = config.duration || 1000,
+                easing = config.easing || 'ease';
+            this.rules = config.rules;
+            for(var i=0,len=rules.length;i<len;i++){
+                var ruleDate=ruleHandler(this.dom,rules[i]);
+                this.prefix[i]=ruleDate.prefix;
+                this.suffix[i]=ruleDate.suffix;
+                this.propertyName[i]=ruleDate.name;
+                this.endValue[i]=ruleDate.endValue;
+                this.startValue[i]=ruleDate.startValue;
+            }
+            this.duration = duration ;
+            this.easing = speedPattern(easing);
+        },
+        start:function(){
+            var self = this,
+                startTime=+new Date;
+            var go=function(){
+                var t=+new Date();
+                if(self.handleCur(t,startTime)){
+                    requestAnimationFrame(function(){
+                        go();
+                    });
+                }
+            };
+            go();
+        },
+        callback:function(){
+            this.func=Array.prototype.shift.apply(arguments);
+            this.args=arguments;
+        }
+    }
+    // AnimateList
+    function AnimateList() {
+        this.length = 0;
+    }
+    AnimateList.prototype = new Array();
+    AnimateList.prototype.start = function () {
+        var startTime=+new Date,
+            animationList = this,
+            curTime,
+            i,
+            len;
+        var go=function(){
+            curTime = +new Date();
+            for( i = 0 , len = animationList.length ; i < len ; i++){
+                if(!animationList[i].handleCur(curTime,startTime)){
+                    animationList.splice(i,1);
+                    len = animationList.length
+                }
+            }
+            if(len>0)
+                requestAnimationFrame(function(){
+                    go();
+                });
+        };
+        go();
+    }
+    AnimateList.prototype.add = function () {
+        var i,
+            len;
+        for(i = 0 , len = arguments.length ; i < len ; i++){
+            if( arguments[i] instanceof  Animate)
+                this.push(arguments[i]);
+        }
+        return this;
+    }
+    AnimateList.prototype.delete = function (animate) {
+        this.splice(this.indexOf(animate),1);
+    }
+    // create animated对象，返回标识符
+    function create(dom,config) {
+        var newAnimate = new Animate();
+        if(dom)
+        	newAnimate.init(dom);
+        if(config) {
+            newAnimate.set(config);
+        }
+        return newAnimate;
+    }
+    // 动画队列控制器
+    function createAnimationList(){
+        var animationList = new AnimateList(),
+            i,
+            len;
+        for( i = 0 , len = arguments.length ; i < len ; i ++){
+            if(arguments[i] instanceof  Animate)
+                animationList.push(arguments[i]);
+        }
+        return animationList;
+    }
+    window.flexibleAniamtion  = {
+        createAnimation:create,
+        createAnimationList:createAnimationList
+    }
+})(window);
 (function() {
     var lastTime = 0;
     var vendors = ['webkit', 'moz'];
     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
         window.cancelAnimationFrame =
-          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+            window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
     }
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(func) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
             var id = window.setTimeout(function() { func(); },
-              timeToCall);
+                timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
